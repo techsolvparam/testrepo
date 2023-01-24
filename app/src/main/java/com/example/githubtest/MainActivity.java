3363e8087b4d18ce0baddde7c78e8b312eaea9ac
techsolvparam <ppanja@techolvit.com>
2023-01-24 10:50:12 +0530

    This is third push.

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "2nd push", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "3rd push", Toast.LENGTH_SHORT).show();
     }
 }
