500b3cde3828f575eb3c460c96dcd0894db6868a
techsolvparam <ppanja@techolvit.com>
2023-01-24 11:01:29 +0530

    This is  push.

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -3,18 +3,27 @@ package com.example.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
+    TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "3rd push", Toast.LENGTH_SHORT).show();
+        init();
+
     }
 
+    public void init(){
+        btn1= (Button) findViewById(R.id.btn);
+        tv1=findViewById(R.id.tv1);
+    }
     public void tech(){
 
     }
